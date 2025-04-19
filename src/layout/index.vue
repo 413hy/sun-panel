86fc702c8dae467e33108b894fdbb47714ac0ea3
13062371708 <3079980988@qq.com>
2025-04-20 03:10:44 +0800

    015

---
+++ src/layout/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { NLayout, NLayoutContent } from 'naive-ui'
+</script>
+
+<template>
+  <NLayout>
+    <NLayoutContent>
+      <router-view />
+    </NLayoutContent>
+  </NLayout>
+</template>
+
+<style scoped>
+.n-layout {
+  min-height: 100vh;
+}
+</style>
